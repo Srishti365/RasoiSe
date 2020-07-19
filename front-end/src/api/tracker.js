30d594fcab05de8d3cde8d717869bf9cfb95d2d6
Sahil0906 <38859827+Sahil0906@users.noreply.github.com>
2020-07-19 10:38:48 +0530

    Update tracker.js

--- front-end/src/api/tracker.js
+++ front-end/src/api/tracker.js
@@ -2,11 +2,7 @@ import axios from 'axios';
 import { AsyncStorage } from 'react-native';
 
 const instance = axios.create({
-<<<<<<< HEAD
     baseURL: 'http://12f23e47e2f7.ngrok.io'
-=======
-    baseURL: 'http://a1d9c59f4ebe.ngrok.io'
->>>>>>> 67d8fb58e0444b0d92f24ebb245782c7e6535c90
 });
 
 instance.interceptors.request.use(
