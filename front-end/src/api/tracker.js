6fdb49ddaf814241cf57b2a91b5835f724b4d2d3
Sahil0906 <sahilchoudhary74306@gmail.com>
2020-07-22 12:00:30 +0530

    Pickup Styling

--- front-end/src/api/tracker.js
+++ front-end/src/api/tracker.js
@@ -2,13 +2,7 @@ import axios from 'axios';
 import { AsyncStorage } from 'react-native';
 
 const instance = axios.create({
-<<<<<<< HEAD
-
     baseURL: 'http://391d092a29c9.ngrok.io'
-
-=======
-    baseURL: 'http://aec6118bbae7.ngrok.io'
->>>>>>> 446efbf44aa6044b18bf1994cc9778b03f4d25a3
 });
 
 instance.interceptors.request.use(
