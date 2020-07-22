e6ee7e5d234f9a19b3aa3bb24a0af0fb62b111fe
chethna19 <mendonchethna@gmail.com>
2020-07-22 13:47:24 +0530

    exec inprocess and history

--- executiveApp/src/Screens/HistoryScreen.js
+++ executiveApp/src/Screens/HistoryScreen.js
@@ -1,10 +1,52 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, ScrollView, FlatList } from 'react-native';
+import trackerApi from '../api/tracker';
+import { NavigationEvents } from 'react-navigation';
 
 const History = () => {
-    return(
+    const [err, setErr] = useState('');
+    const [result, setResult] = useState([]);
+
+    const viewHistory = async () => {
+        try {
+            const response = await trackerApi.get('/execdetails/viewdelivered');
+            setResult(response.data.orders);
+
+        }
+        catch (err) {
+            console.log(err);
+            setErr('Something went wrong');
+        }
+    }
+
+    useEffect(() => {
+        viewHistory();
+    }, []);
+
+    // console.log(result);
+
+
+    return (
         <View>
-            <Text>History Screen</Text>
+            <NavigationEvents onDidFocus={() => viewHistory()} />
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <FlatList
+                    showsVerticalScrollIndicator
+                    data={result}
+                    keyExtractor={(result) => result._id}
+                    renderItem={({ item }) => {
+                        return (
+                            <View style={{ marginHorizontal: 15, marginVertical: 15 }}>
+                                <Text>Delivered to: {item.user.name}</Text>
+                                <Text>Delivery address: {item.delivery_add}</Text>
+                                <Text>Customer contact: {item.user.phoneNo}</Text>
+                                <Text>Customer email: {item.user.email}</Text>
+
+                            </View>
+                        )
+                    }}
+                />
+            </ScrollView>
         </View>
     )
 }
