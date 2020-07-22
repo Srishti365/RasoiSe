4f28b13634259687b097ecc38171c61aa154a826
Sahil0906 <sahilchoudhary74306@gmail.com>
2020-07-22 18:53:57 +0530

    Map Screen

---
+++ executiveApp/src/Components/Map.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet, Dimensions } from 'react-native';
+import MapView, { Polyline, Marker } from 'react-native-maps'
+const polyline = require('@mapbox/polyline');
+
+const Map = ({ data, pickup_loc, dest_loc, exec_loc }) => {
+
+    let points = polyline.decode(data)
+    let coords = points.map((point, index) => ({ latitude: point[0], longitude: point[1] }))
+
+    console.log(pickup_loc,dest_loc,exec_loc)
+
+    return (
+        <View style={styles.container}>
+            <MapView style={styles.mapStyle}
+                initialRegion={{
+                    latitude: pickup_loc[0],
+                    longitude: pickup_loc[1],
+                    latitudeDelta: 0.01,
+                    longitudeDelta: 0.01
+                }}
+            >
+                <Polyline
+                    coordinates={coords}
+                    strokeColor="red"
+                    strokeWidth={3} 
+                />
+
+                <Marker coordinate={{ latitude: exec_loc[0], longitude: exec_loc[1] }} />
+                <Marker coordinate={{ latitude: dest_loc[0], longitude: dest_loc[1] }} />
+                <Marker coordinate={{ latitude: pickup_loc[0], longitude: pickup_loc[1] }} />
+
+
+            </MapView>
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    mapStyle: {
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
+    },
+})
+
+export default Map;
