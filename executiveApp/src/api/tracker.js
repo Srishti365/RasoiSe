84273cbd1e7abef7abf5cc1bf31340b5b7f7bcc6
Srishti365 <srishti.s365@gmail.com>
2020-07-23 22:29:12 +0530

    search relevance

--- executiveApp/src/api/tracker.js
+++ executiveApp/src/api/tracker.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { AsyncStorage } from 'react-native';
 
 const instance = axios.create({
-    baseURL: 'http://88f0656a95b3.ngrok.io'
+    baseURL: 'http://3dcd8244c640.ngrok.io'
 });
 
 instance.interceptors.request.use(
